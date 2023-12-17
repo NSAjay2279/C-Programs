a539da7f3265829f6c7f53742d055510c606d3d2
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-17 07:27:41 +0000

    Sun, Dec 17, 2023, 12:57 PM +05:30

--- true.c
+++ true.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_size();
+
 int main(void)
 {
     int n = get_size();
     printf("%d",n);
 }
 
-int get_size(void)
+int get_size()
 {
     return 4;
 }
