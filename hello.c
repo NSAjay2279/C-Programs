b4cb5e2d17a4910824a5b484cc3dcaac9c8c7a2a
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-17 05:31:56 +0000

    Sun, Dec 17, 2023, 11:01 AM +05:30

--- hello.c
+++ hello.c
@@ -5,9 +5,7 @@ int main(void)
 
 {
     while(true) {
-        int x = 1;
-        x++;
-        printf("Working? %d , yes!", x);
-        x++;
+        int x = 1/0;
+        printf("%d", x);
     }
 }
