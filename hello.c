2fe9ab3015d349ea9f8031814da0b8b160ddb085
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-17 03:47:55 +0000

    Sun, Dec 17, 2023, 9:17 AM +05:30

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+import java.util.Scanner;
 
 int main(void)
 
 {
     printf("Hello, World\n");
+    scanner scan = new Scanner(Sytem.in);
+    string s = scan.nextLine();
+    printf("Working? %s , yes!", s);
 }
