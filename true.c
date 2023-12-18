e96e2ccb0fdbb98c448eee669e10fd4ccb0c6b74
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-18 06:01:28 +0000

    Mon, Dec 18, 2023, 11:31 AM +05:30

--- true.c
+++ true.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-#include <cs50.h>
 
+int main() {
+  // Declare an array of integers
+  int numbers[] = {1, 2, 3, 4, 5};
 
-int main(void)
-{
-    int x = 30;
-    int y = 50;
-    printf(" %i ", x+y);
+  // Get the size of the array in bytes
+  size_t array_size = sizeof(numbers);
+
+  // Get the size of one element in bytes
+  size_t element_size = sizeof(numbers[0]);
+
+  // Calculate the number of elements in the array
+  int num_elements = array_size / element_size;
+
+  // Print the size of the array in bytes and number of elements
+  printf("Size of the array in bytes: %zu\n", array_size);
+  printf("Number of elements in the array: %d\n", num_elements);
+
+  return 0;
 }
-
