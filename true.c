461879571732c7381f14040e282b3e0ce7bb249d
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-18 09:12:26 +0000

    Mon, Dec 18, 2023, 2:42 PM +05:30

--- true.c
+++ true.c
@@ -3,7 +3,21 @@
 #include <cs50.h>
 
 int main() {
-   string s = get_string("enter a string: ");
-   int n = strlen(s);
-   printf("%i",n);
+    string s = get_string("Enter a string: ");
+
+    // Initialize word count to 1 because the first word doesn't have a preceding space
+    int wordCount = 1;
+
+    // Iterate through the characters in the string
+    for (int i = 0; i < strlen(s); i++) {
+        // Check for space character to identify word boundaries
+        if (s[i] == ' ') {
+            // Increment word count when a space is encountered
+            wordCount++;
+        }
+    }
+
+    printf("Number of words: %i\n", wordCount);
+
+    return 0;
 }
