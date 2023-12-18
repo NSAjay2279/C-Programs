64e0741947009febc3e91cb80a3acea8031b11d5
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-18 09:04:48 +0000

    Mon, Dec 18, 2023, 2:34 PM +05:30

--- true.c
+++ true.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    // Declare an array
-    int myArray[] = {1, 2, 3, 4, 5};
-
-    // Calculate the length of the array
-    int arrayLength = sizeof(myArray) / sizeof(myArray[0]);
-
-    // Print the length of the array
-    printf("Length of the array: %d\n", arrayLength);
-
-    return 0;
+   string s = "Hello buddy";
+   printf("%i",strlen(s));
 }
