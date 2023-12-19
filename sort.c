aedd714a879a17c83aa9c615ba5d647ede2a6d2b
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-19 12:20:34 +0000

    Tue, Dec 19, 2023, 5:50 PM +05:30

--- sort.c
+++ sort.c
@@ -2,9 +2,22 @@
 #include <cs50.h>
 
 int main(void) {
-    int n = get_int("Enter the value of n");
+    int n = get_int("Enter the value of n\n");
     for(int i=0; i<n; i++){
-        int num[] = get_int("enter the %i numbers", n);
-        
+        int num[i] = get_int("enter the %i numbers\n\n", n);
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=1; i<=n; j++){
+            num[i] < num[i+j];
+        }
+        int temp[i] = num[i];
+        num[i+j] = num[i];
+        num[i] = temp[i];
+    }
+
+    printf("Here are the sorted numbers: ");
+    for(int i=0; i<n; i++){
+        printf("%i", i);
     }
 }
