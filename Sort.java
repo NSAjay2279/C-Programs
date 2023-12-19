03e8af9d5347cdddc44fdb2168c903bd8696fc22
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-19 12:38:43 +0000

    Tue, Dec 19, 2023, 6:08 PM +05:30

---
+++ Sort.java
@@ -0,0 +1,39 @@
+java.util.*;
+
+class Sort {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter n value: ");
+        int n = scan.nextInt();
+
+        int num[n];
+        System.out.println("Enter " + n + "numbers");
+        for(int i=0; i<n; i++) {
+            num[i] = scan.nextInt();
+        }
+    }
+}
+
+/*
+int main(void) {
+    int n = get_int("Enter the value of n\n");
+    int num[n];
+    for(int i=0; i<n; i++){
+        int num[i] = get_int("enter the %i numbers: ", i+1);
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=1; i<=n; j++){
+            num[i] < num[i+j];
+        }
+        int temp[i] = num[i];
+        num[i+j] = num[i];
+        num[i] = temp[i];
+    }
+
+    printf("Here are the sorted numbers: ");
+    for(int i=0; i<n; i++){
+        printf("%i", i);
+    }
+}
