89eb356a43dfb8863b80f9c1c3f3f259e911be3a
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-19 12:11:41 +0000

    Tue, Dec 19, 2023, 5:41 PM +05:30

--- sort.c
+++ sort.c
@@ -2,5 +2,9 @@
 #include <cs50.h>
 
 int main(void) {
-    string 
+    int n = get_int("Enter the value of n");
+    for(int i=0; i<n; i++){
+        int num[] = get_int("enter the %i numbers", n);
+        
+    }
 }
