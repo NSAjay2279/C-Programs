65ff94a9760fd1a550be463fc9b879c9e7a27d15
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-20 05:22:04 +0000

    Wed, Dec 20, 2023, 10:52 AM +05:30

--- bubblesort.c
+++ bubblesort.c
@@ -1,35 +1,41 @@
-import java.util.*;
+#include <stdio.h>
 
-class Sort {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Enter n value: ");
-        int n = scan.nextInt();
-
-        int num[] = new int[n];
-        System.out.println("Enter " + n + " numbers");
-        for(int i=0; i<n; i++) {
-            num[i] = scan.nextInt();
-        }
-
-        bubbleSort(int n, int num[n]);
-
-        System.out.println("Here are the sorted numbers: ");
-        for(int i=0; i<n; i++){
-            System.out.print(num[i] + " ");
-        }
-    }
-
-    public int bubbleSort(int n, int num[n]){
-        for(int i=0; i<n; i++) {
-            {
-                if (num[i] > num[i+1]) {
-                    int temp = num[i];
-                    num[i] = num[i+1];
-                    num[i+1] = temp;
-                }
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap if the element found is greater
+            // than the next element
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Example usage
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
 }
---
+++ selection.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// Function to perform Selection Sort
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+    for (i = 0; i < n - 1; i++) {
+        // Assume the current index is the minimum
+        minIndex = i;
+
+        // Find the index of the minimum element in the unsorted part
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Example usage
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
