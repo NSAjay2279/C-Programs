d91af04aa52ffdee0ea76575864d19aecdfac4eb
A-Java-Man <144517147+A-Java-Man@users.noreply.github.com>
2023-12-21 08:04:28 +0000

    Thu, Dec 21, 2023, 1:34 PM +05:30

--- bubble.c
+++ bubble.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
 
-// Function to perform Bubble Sort
 void bubbleSort(int arr[], int n) {
-    int temp;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j <= n - 2; j++) {
-            // Swap if the element found is greater
-            // than the next element
+    int i, j, swapped;
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;  // Flag to check if any swaps occurred
+
+        for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
+                // Swap elements
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+
+                swapped = 1;  // Indicate that a swap occurred
             }
+        }
+
+        // If no swaps occurred in this pass, the array is already sorted
+        if (!swapped) {
+            break;
         }
     }
 }
 
-// Function to print an array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-// Example usage
 int main() {
-    int arr[] = {64, 34, 25, 12};
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Original array: ");
-    printArray(arr, n);
+    printf("Unsorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     bubbleSort(arr, n);
 
-    printf("Sorted array: ");
-    printArray(arr, n);
+    printf("\nSorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
