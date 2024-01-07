f239091b6cc2adcd692734b1aa1cf6699830f819
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-01-07 08:28:10 +0000

    Sun, Jan 7, 2024, 1:58 PM +05:30

--- binary.c
+++ binary.c
@@ -4,13 +4,16 @@ int b = 2; #32 bits - 0000........10
 int c = 3; #32 bits - 000000000000100
 
 #convert a = 1 to 00000...0001
-void converter(parameter)
-{
-
-}
+void converter(parameter);
 
 int main()
 {
     int num = scanf("Enter Your number: ");
     converter(num)
+}
+
+void converter(parameter);
+{
+    1 -->> 0000....0001
+    
 }
